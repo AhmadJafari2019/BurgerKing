e521ee746bdf0638d89b714db075d33272db2a86
Ahmad <ahmad.hazara@gmx.ch>
2019-12-22 23:21:14 +0100

    Adding the BuildControl Component and BuildControl.module.css and adding the css inside it for the BuildControl

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const buildControl = (props) => (
     <div>
-        
+        <div>{props.label}</div>
+        <button>Less</button>
+        <button>More</button>
     </div>
 );
 
