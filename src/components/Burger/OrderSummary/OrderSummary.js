26fe24ee791f68d8470794d4892afea269a8e6ee
Ahmad <ahmad.hazara@gmx.ch>
2019-12-23 23:15:01 +0100

    Adding the Purchase Button to the OrderSummary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Aux from '../../../hoc/Aux';
-
+import Button from '../../UI/Button/Button';
 const orderSummary = props => {
   const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
     return (
@@ -15,10 +15,14 @@ const orderSummary = props => {
     <Aux>
       <h3>Your Order</h3>
       <p>A delicious burger with the following ingredients:</p>
-      <ul>
-        {ingredientSummary}
-        <p>Continue to cheek out?</p>
-      </ul>
+      <ul>{ingredientSummary}</ul>
+      <p>Continue to cheek out?</p>
+      <Button btnType="Danger" clicked={props.purchaseCancelled}>
+        CANCEL
+      </Button>
+      <Button btnType="Success" clicked={props.purchaseContinued}>
+        CONTINUE
+      </Button>
     </Aux>
   );
 };
