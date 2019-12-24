f29a77e5401966e363ea98c6f615bda3c951391a
Ahmad <ahmad.hazara@gmx.ch>
2019-12-24 13:46:29 +0100

    Adding the Backdrop for the Responsive mb

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,13 +2,19 @@ import React from 'react';
 import Logo from '../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from '../SideDrawer/SideDrawer.module.css';
-import Backdrop from '../SideDrawer/SideDrawer';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 import Aux from '../../../hoc/Aux';
 const sideDrawer = props => {
+  let attachedClasses = [classes.SideDrawer, classes.Close];
+
+  if (props.open) {
+    attachedClasses = [classes.SideDrawer, classes.Open];
+  }
+
   return (
     <Aux>
-      <Backdrop show />
-      <div className={classes.SideDrawer}>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={attachedClasses.join(' ')}>
         <div className={classes.Logo}>
           <Logo />
         </div>
