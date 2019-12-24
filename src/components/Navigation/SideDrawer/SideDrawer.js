526011e86268bb904b6cc47fe71931d78214e0a6
Ahmad <ahmad.hazara@gmx.ch>
2019-12-24 12:38:10 +0100

    Responsive Adjusment.

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,14 +2,21 @@ import React from 'react';
 import Logo from '../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from '../SideDrawer/SideDrawer.module.css';
+import Backdrop from '../SideDrawer/SideDrawer';
+import Aux from '../../../hoc/Aux';
 const sideDrawer = props => {
   return (
-    <div className={classes.SideDrawer}>
-      <Logo />
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    <Aux>
+      <Backdrop show />
+      <div className={classes.SideDrawer}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+    </Aux>
   );
 };
 
